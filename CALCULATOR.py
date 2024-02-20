74d7eef24f6d629cd3e39228c73708e63e251ff6
darshan90226 <157240461+darshan90226@users.noreply.github.com>
2024-02-20 09:53:06 +0530

    Update CALCULATOR.py
    Signed-off-by: darshan90226 <157240461+darshan90226@users.noreply.github.com>

--- CALCULATOR.py
+++ CALCULATOR.py
@@ -2,7 +2,7 @@ print("_calculator_")
 num1 = float(input("enter the first number: "))
 num2 = float(input("enter the second number: "))
 print("press 1 for addition\npress 2 for subtraction\npress 3 for multiplication\npress 4 for division ")
-choice = int(input("entre your choice from 1-5: "))
+choice = int(input("entre your choice from 1-4: "))
 if choice ==1:
     result = num1+num2
     print(f"The addition of the number is: {result}  ")
@@ -15,5 +15,5 @@ elif choice ==3:
 elif choice ==4:
     result = num1/num2
     print(f"The division of the two number is :  {result}")   
-print("Thank you")
+print("Thank you") 
          
